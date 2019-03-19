7d7695815c60de6f46858531f5b0b06302258421
Cristian Felipe Buitrago Rodriguez <36422189+buty619@users.noreply.github.com>
2019-03-19 13:30:09 -0500

    p

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.post('/notes', function(req,res){
   res.redirect("/notes");
 });
 
-app.get('/notes/:id', function(req,res){
+app.get('/notes/:id', async function(req,res){
   const notas = await Notes.find();
   const note = await Notes.findById(req.params.id);
   res.render("show",{notes:notas, currentNote: note});
