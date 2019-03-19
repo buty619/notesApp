9fb2390586ab972b4292387685047073f6f3a759
Cristian Felipe Buitrago Rodriguez <36422189+buty619@users.noreply.github.com>
2019-03-18 21:56:03 -0500

    p

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,7 @@
 $("#text").keypress(function (e) {           
     var key = e.which;        
     if (key == 13) {
-      $("#allNotes").append("<h2>"+$("#title").val()+"</h2><p>"+$("#text").val()+"</p>")
+      //$("#allNotes").append("<h2>"+$("#title").val()+"</h2><p>"+$("#text").val()+"</p>")
       $("form").submit();
       $("#text").val("");
       $("#title").val("");      
