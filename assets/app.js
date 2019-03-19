3c84c6c6988dd2de972412c8450329025c92e23a
Cristian Felipe Buitrago Rodriguez <36422189+buty619@users.noreply.github.com>
2019-03-18 21:23:41 -0500

    push

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,7 @@
-$("#text").keypress(function (e) {
-    e.preventDefault();        
-    var key = e.which;        
-    if (key == 13) {
+$('form').submit(function(e){
+      e.preventDefault();
       $("allNotes").append("<h2>"+$("#title").val()+"</h2><p>"+$("#text").val()+"</p>")
       $("form").submit();
       $("#text").val("");
       $("#title").val("");      
-    }
 });
