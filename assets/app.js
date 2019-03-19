f493d5fdac23e78f96c1bd89e5c62873e1c549d6
Cristian Felipe Buitrago Rodriguez <36422189+buty619@users.noreply.github.com>
2019-03-18 22:10:16 -0500

    p

--- assets/app.js
+++ assets/app.js
@@ -1,16 +1,8 @@
 $("#text").keypress(function (e) {           
     var key = e.which;        
     if (key == 13) {
-      //$("#allNotes").append("<h2>"+$("#title").val()+"</h2><p>"+$("#text").val()+"</p>")
       $("form").submit();
       $("#text").val("");
       $("#title").val("");      
     }
 });
-
-// $('form').submit(function(e){
-//     e.preventDefault();
-//     socket.emit('chat message', $('#m').val());
-//     $('#m').val('');
-//     return false;
-//   });
